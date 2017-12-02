a0b19eaaae616555482e8affb23798aa0bbaf0e0
nhwlt <nhwlt@DESKTOP-NSEE0H6.sas.local>
2017-12-02 13:31:25 -0500

    Added LogitechController.java and XboxController.java to controller2018 package

---
+++ Getting Started/src/controller2018/XboxController.java
@@ -0,0 +1,77 @@
+package controller2018;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class XboxController extends Joystick{
+
+	public XboxController(int port) {
+		super(port);
+	}
+	
+	public boolean getAButton() {
+		return this.getRawButton(1);
+	}
+	
+	public boolean getBButton() {
+		return this.getRawButton(2);
+	}
+	
+	public boolean getXButton() {
+		return this.getRawButton(3);
+	}
+	
+	public boolean getYButton() {
+		return this.getRawButton(4);
+	}
+	
+	public boolean getLB() {
+		return this.getRawButton(5);
+	}
+	
+	public boolean getRB() {
+		return this.getRawButton(6);
+	}
+	
+	public boolean getBackButton() {
+		return this.getRawButton(7);
+	}
+	
+	public boolean getStartButton() {
+		return this.getRawButton(8);
+	}
+	
+	public boolean getL3() {
+		return this.getRawButton(9);
+	}
+	
+	public boolean getR3() {
+		return this.getRawButton(10);
+	}
+	
+	public double getLS_X() {
+		return this.getRawAxis(0);
+	}
+	
+	public double getLS_Y() {
+		return this.getRawAxis(1);
+	}
+	
+	public double getRS_X() {
+		return this.getRawAxis(4);
+	}
+	
+	public double getRS_Y() {
+		return this.getRawAxis(5);
+	}
+	
+	public double getTriggerRight() {
+		return this.getRawAxis(3);
+	}
+	public double getTriggerLeft() {
+		return this.getRawAxis(2);
+	}
+	
+	
+	
+	
+}
