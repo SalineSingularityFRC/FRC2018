367c0d27fcc8587bc9ac5d8d2042d397eb66f5e2
JWebb22 <webbj@salinehornets.org>
2018-01-15 15:56:49 -0500

    Started on Lift mechanism

---
+++ src/org/usfirst/frc/team5066/robot/Lift.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team5066.robot;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class Lift {
+	
+	//TODO:
+	//work off singularity
+	//limit switches
+	//create method
+	//run motor until limit switch
+	//create method
+	//run until other limit switch
+	//dont do while loop
+	//be running continously
+	//unlock button
+	//button to raise
+	//button to unlock
+	//do javabat when done
+	
+	private CANTalon right1, right2, left1, left2;
+	DigitalInput rightLim1, rightLim2, leftLim1, leftLim2;
+	
+	public Lift(int r1, int r2, int l1, int l2, int rL1, int rL2) {
+		
+		
+		
+	}
+
+}
