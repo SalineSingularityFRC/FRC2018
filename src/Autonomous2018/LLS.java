5ff03c4cdf3a1ad61cfb8a23b13f553360a67c81
lmah7 <lmah7@DESKTOP-7LHURO7.sas.local>
2018-01-16 19:55:18 -0500

    Created auton left left switch, auton left double switch.

---
+++ src/Autonomous2018/LLS.java
@@ -0,0 +1,24 @@
+package Autonomous2018;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+import edu.wpi.first.wpilibj.SPI.Port;
+
+public class LLS extends AutonControlScheme{
+
+	public LLS(SingDrive drive, Port gyroPort) {
+		super(drive, gyroPort);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void MoveAuton() {
+		// TODO Auto-generated method stub
+		super.vertical(168);
+		//lift arm
+		super.rotate(90, false);
+		//place block
+	}
+
+}
