f946c6b3cee6c2919d2d5f76622acaa6a2d9e080
lmah7 <lmah7@DESKTOP-7LHURO7.sas.local>
2018-01-22 19:41:47 -0500

    Added 3d array to robot.java

--- src/org/usfirst/frc/team5066/robot/Robot.java
+++ src/org/usfirst/frc/team5066/robot/Robot.java
@@ -132,15 +132,48 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		/*Object[][] L = {{new LLSLS(drive), new LLSV(drive), new LLSOL(drive), new LLSOR(drive)},
-		{new LRSRS(drive), new LRSV(drive), new LRSOL(drive), new LRSOR(drive)}};
+		Object[][][] autonPrograms = {{{new LLSLS(drive), new LLSV(drive), new LLSOL(drive), new LLSOR(drive)},
+			{new LRSRS(drive), new LRSV(drive), new LRSOL(drive), new LRSOR(drive)}},{{new RRSRS(drive), 
+			new RRSV(drive), new RRSOL(drive), new RRSOR(drive)},{new RLSLS(drive), new RLSV(drive), new RLSOL(drive),
+			new RLSOR(drive)}},{{new MLSLS(drive), new MLSV(drive), new MLSOL(drive), new MLSOR(drive)},{new MRSRS(drive),
+			new MRSV(drive), new MRSOL(drive), new MRSOL(drive)}}};
 
-		String gameData;
-		gameData = DriverStation.getInstance().getGameSpecificMessage();
-
-		autonomousCommand = (Command) autoChooser.getSelected();
-		autonomousCommand.start();
-		 */
+			String gameData;
+			int a=0,b=0,c=0;
+			gameData = DriverStation.getInstance().getGameSpecificMessage();		
+			
+			if(side.equals(new RLSLS(drive))){
+				a=1;
+				SmartDashboard.putString("DB/String 0", "Starting Right");
+			}
+			
+			if(side.equals(new MLSLS(drive))){
+				a=2;
+				SmartDashboard.putString("DB/String 0", "Stating Middle");
+			}
+			
+			if(gameData.charAt(0) == 'R'){
+				b=1;
+				SmartDashboard.putString("DB/String 1", "Our Switch is on the right");
+			}
+			
+			if(priority.equals(new LLSV(drive))){
+				c=1;
+				SmartDashboard.putString("DB/String 2", "We are prioritizing the vault");
+			}
+			if(priority.equals(new LLSOL(drive))){
+				c=2;
+				SmartDashboard.putString("DB/String 2", "Moving to opponent Left");
+			}
+			if(priority.equals(new LLSOR(drive))){
+				c=3;
+				SmartDashboard.putString("DB/String 2", "Moving to opponent Right");
+			}
+			
+			
+			
+			autonomousCommand = (Command) autonPrograms[a][b][c];
+			autonomousCommand.start();
 	}
 
 	/**
