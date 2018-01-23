f21c84e8f92aafdae167fc8038dde55605299ef7
lmah7 <lmah7@DESKTOP-7LHURO7.sas.local>
2018-01-22 20:12:21 -0500

    RLSV class

---
+++ src/org/usfirst/frc/team5066/autonomous2018/RLSV.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team5066.autonomous2018;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+public class RLSV extends AutonControlScheme {
+
+	public RLSV(SingDrive drive) {
+		super(drive);
+	}
+
+	@Override
+	public void moveAuton() {
+		super.vertical(35);
+		super.rotate(90, true);
+		super.vertical(190.15-super.CenterRobotLength);
+		super.rotate(90, false);
+		super.vertical(105-super.CenterRobotLength);
+		//Drop PC
+		super.vertical(- (32.5-super.CenterRobotWidth));
+		//Lower PC manipulator
+		super.rotate(90, false);
+		super.vertical(47.5-super.CenterRobotLength);
+		//pick up PC
+		super.rotate(180, false);
+		super.vertical(24-super.CenterRobotLength);
+		super.rotate(90, true);
+		super.vertical(63.5-super.CenterRobotLength);
+	}
+
+}
