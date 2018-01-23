690faff23ad7073e31919bd735596d4547303f4f
lmah7 <lmah7@DESKTOP-7LHURO7.sas.local>
2018-01-22 19:58:55 -0500

    Added RLSOR and RRSOR

---
+++ src/org/usfirst/frc/team5066/autonomous2018/RLSOR.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team5066.autonomous2018;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+public class RLSOR extends AutonControlScheme {
+
+	public RLSOR(SingDrive drive) {
+		super(drive);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void moveAuton() {
+		super.vertical(35);
+		super.rotate(90, true);
+		super.vertical(221.06-super.CenterRobotWidth+super.CenterRobotLength);
+		super.rotate(90, false);
+		super.vertical(133-super.CenterRobotLength);
+		super.rotate(90, false);
+		//raise PC
+		super.vertical(12);
+		//Drop PC
+		super.vertical(- (32.5-super.CenterRobotWidth));
+		super.rotate(90, false);
+		//Lower PC manipulator
+		super.vertical(54-super.CenterRobotLength);
+		//pick up PC
+		super.rotate(90, false);
+		super.vertical(super.CenterRobotLength-13);
+		super.rotate(90, true);
+		super.vertical(60.5+super.CenterRobotLength);
+		super.rotate(90, true);
+		super.vertical(261.5);
+
+	}
+
+}
