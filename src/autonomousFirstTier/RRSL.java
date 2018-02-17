a286a58bb686e455b71618352ad71e813697f716
Alex Mah <maxhale4@gmail.com>
2018-02-17 11:21:04 -0500

    Created 1 tier package

---
+++ src/autonomousFirstTier/RRSL.java
@@ -0,0 +1,23 @@
+package autonomousFirstTier;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.robot.Arm;
+import org.usfirst.frc.team5066.robot.Intake;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class RRSL extends AutonControlScheme {
+
+	public RRSL(SingDrive drive, AHRS gyro, Arm arm, Intake intake) {
+		super(drive, gyro, arm, intake);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void moveAuton() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
