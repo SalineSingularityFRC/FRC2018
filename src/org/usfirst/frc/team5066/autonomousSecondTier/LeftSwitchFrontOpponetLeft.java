2a9e9d918df5d1390d7ba96ad64b24c52005d00a
JWebb22 <webbj@salinehornets.org>
2018-02-17 14:28:01 -0500

    Completed teir2 LeftSwitchFront

---
+++ src/org/usfirst/frc/team5066/autonomousSecondTier/LeftSwitchFrontOpponetLeft.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team5066.autonomousSecondTier;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.robot.Arm;
+import org.usfirst.frc.team5066.robot.Intake;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class LeftSwitchFrontOpponetLeft extends AutonControlScheme {
+
+	public LeftSwitchFrontOpponetLeft(SingDrive drive, AHRS gyro, Arm arm, Intake intake) {
+		super(drive, gyro, arm, intake);
+	}
+
+	@Override
+	public void moveAuton() {
+		super.verticalReverse(super.CenterRobotCorner+5, Arm.Position.SWITCH, false);
+		super.rotate(90, true,Arm.Position.TRAVEL);
+		super.vertical(30+super.CenterRobotWidth, Arm.Position.TRAVEL, false);
+		super.rotate(90, false);
+		super.vertical(86+super.CenterRobotCorner+super.CenterRobotWidth);
+		super.rotate(90, false);
+		super.vertical(18.5);
+		super.rotate(90, false, Arm.Position.PICKUP);
+		super.vertical(25, Arm.Position.PICKUP, true);
+		super.verticalReverse(25, Arm.Position.TRAVEL, false);
+		super.rotate(90, false, Arm.Position.TRAVEL);
+		super.vertical(31.75);
+		super.rotate(90, false);
+		super.vertical(154-super.CenterRobotLength);
+	}
+
+}
