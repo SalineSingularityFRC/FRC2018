cf73c30bfa2eed30a90570a7ddcbd12cacb7cc8f
Alex Mah <maxhale4@gmail.com>
2018-02-16 21:48:13 -0500

    added LLT and LRT.

---
+++ src/LRT.java
@@ -0,0 +1,21 @@
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.robot.Arm;
+import org.usfirst.frc.team5066.robot.Intake;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class LRT extends AutonControlScheme {
+
+	public LRT(SingDrive drive, AHRS gyro, Arm arm, Intake intake) {
+		super(drive, gyro, arm, intake);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void moveAuton() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/usfirst/frc/team5066/autonomous2018/LLT.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team5066.autonomous2018;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.robot.Arm;
+import org.usfirst.frc.team5066.robot.Intake;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+
+public class LLT extends AutonControlScheme{
+
+	public LLT(SingDrive drive, AHRS gyro, Arm arm, Intake intake) {
+		super(drive, gyro, arm, intake);
+	}
+
+	@Override
+	public void moveAuton() {
+		super.vertical(100-super.CenterRobotLength, Arm.Position.TRAVEL, false);
+		super.vertical(220, Arm.Position.HIGHSCALE, false);
+		super.rotate(90, true, Arm.Position.HIGHSCALE);
+		intake.autonOuttake();
+	}
+
+}
