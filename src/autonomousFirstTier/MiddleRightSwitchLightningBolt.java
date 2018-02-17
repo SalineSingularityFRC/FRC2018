5990e6fbbed9648b533a1c50cc24d160341e3530
brand <brand@LAPTOP-1PF3NT74.sas.local>
2018-02-17 11:48:05 -0500

    MiddleRightSwitchLightingBolt added

---
+++ src/autonomousFirstTier/MiddleRightSwitchLightningBolt.java
@@ -0,0 +1,28 @@
+package autonomousFirstTier;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.robot.Arm;
+import org.usfirst.frc.team5066.robot.Intake;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class MiddleRightSwitchLightningBolt extends AutonControlScheme {
+	
+	public MiddleRightSwitchLightningBolt(SingDrive drive, AHRS gyro, Arm arm, Intake intake) {
+		super(drive, gyro, arm, intake);
+		
+	@Override
+	public void moveAuton() {
+		//Lightning bolt to right switch
+		super.vertical(48, Arm.Position.TRAVEL, false);
+		super.rotate(90, false, Arm.Position.TRAVEL);
+		super.vertical(66-super.CenterRobotWidth, Arm.Position.TRAVEL, false);
+		//rotate so robot is facing the switch
+		super.rotate( 90, true, Arm.Position.SWITCH);
+		super.vertical(95 - super.CenterRobotLength, Arm.Position.SWITCH, false);
+		intake.autonOuttake();
+
+	}
+
+}
