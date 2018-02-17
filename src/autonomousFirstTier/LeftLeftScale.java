2ec9a23ad944e0596c8a08d2bf29af36c9bd788f
Alex Mah <maxhale4@gmail.com>
2018-02-17 11:52:32 -0500

    Added left scenarios to tier1

---
+++ src/autonomousFirstTier/LeftLeftScale.java
@@ -0,0 +1,24 @@
+package autonomousFirstTier;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.robot.Arm;
+import org.usfirst.frc.team5066.robot.Intake;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class LeftLeftScale extends AutonControlScheme {
+
+	public LeftLeftScale(SingDrive drive, AHRS gyro, Arm arm, Intake intake) {
+		super(drive, gyro, arm, intake);
+	}
+
+	@Override
+	public void moveAuton() {
+		super.vertical(100-super.CenterRobotLength, Arm.Position.TRAVEL, false);
+		super.vertical(220, Arm.Position.HIGHSCALE, false);
+		super.rotate(90, true, Arm.Position.HIGHSCALE);
+		intake.autonOuttake();
+	}
+
+}
---
+++ src/autonomousFirstTier/LeftRightScale.java
@@ -0,0 +1,27 @@
+package autonomousFirstTier;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.robot.Arm;
+import org.usfirst.frc.team5066.robot.Intake;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class LeftRightScale extends AutonControlScheme {
+
+	public LeftRightScale(SingDrive drive, AHRS gyro, Arm arm, Intake intake) {
+		super(drive, gyro, arm, intake);
+	}
+
+	@Override
+	public void moveAuton() {
+		super.vertical(261 - super.CenterRobotLength, Arm.Position.TRAVEL, false);
+		super.rotate(90, true, Arm.Position.TRAVEL);
+		super.vertical(182, Arm.Position.HIGHSCALE, false);
+		super.rotate(90, false, Arm.Position.HIGHSCALE);
+		super.verticalReverse(38, Arm.Position.HIGHSCALE, false);
+		intake.autonOuttake();
+
+	}
+
+}
