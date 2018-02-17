8aac6848f8275ab57605223b2c2ae9f49f61d3ba
haraldureg <haraldureg@gmail.com>
2018-02-17 14:25:00 -0500

    RighSwitchSideLeftScale added

--- src/org/usfirst/frc/team5066/autonomousSecondTier/RightSwitchSideLeftScale.java
+++ src/org/usfirst/frc/team5066/autonomousSecondTier/RightSwitchSideLeftScale.java
@@ -21,6 +21,17 @@ public class RightSwitchSideLeftScale extends AutonControlScheme {
 		intake.autonOuttake();
 		super.verticalReverse(super.CenterRobotCorner, Arm.Position.SWITCH, false);
 		super.rotate(90, false);
+		super.vertical((52/2)+2+13+super.CenterRobotWidth, Arm.Position.TRAVEL, false);
+		super.rotate(90, true, Arm.Position.PICKUP);
+		super.vertical(super.CenterRobotLength);
+		super.rotate(90, true);
+		super.vertical(13,Arm.Position.PICKUP,true);
+		super.verticalReverse(13+1,Arm.Position.PICKUP, false);
+		super.rotate(90,false);
+		super.vertical((264+26.69+26.69)-95.25*2);
+		super.rotate(90,true,Arm.Position.LEVELSCALE);
+		super.verticalReverse(299.65-(261.47+13+super.CenterRobotCorner),Arm.Position.LEVELSCALE, false);
+		intake.autonOuttake();
 	}
 
 }
