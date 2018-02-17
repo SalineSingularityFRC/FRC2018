a8fc90305422cab44a5c3fa46e4ebb39636e4ab7
haraldureg <haraldureg@gmail.com>
2018-02-17 14:56:14 -0500

    Added RightSwitchSideRightSwitchWorking (working bc eclipse ignored in git staging)

---
+++ src/org/usfirst/frc/team5066/autonomousSecondTier/RightSwitchSideRightSwitchWorking.java
@@ -0,0 +1,35 @@
+package src.org.usfirst.frc.team5066.autonomousSecondTier;
+
+import org.usfirst.frc.team5066.controller2018.AutonControlScheme;
+import org.usfirst.frc.team5066.robot.Arm;
+import org.usfirst.frc.team5066.robot.Intake;
+import org.usfirst.frc.team5066.singularityDrive.SingDrive;
+
+public class RightSwitchSideRightSwitchWorking extends AutonControlScheme {
+	
+	public RightSwitchSideRightSwitchWorking(SingDrive drive, AHRS gyro, Arm arm, Intake intake) {
+		super(drive, gyro, arm, intake);
+	}
+
+	@Override
+	public void moveAuton() {
+		// TODO Auto-generated method stub
+		super.vertical(168-super.CenterRobotLength);
+		super.rotate(90, true, Arm.Position.SWITCH);
+		super.vertical(super.CenterRobotWidth, Arm.Position.SWITCH, false);
+		intake.autonOuttake();
+		super.verticalReverse(super.CenterRobotCorner+1, Arm.Position.SWITCH, false);
+		super.rotate(90, true);
+		super.vertical(56/2+super.CenterRobotLength);
+		super.rotate(90, false,Arm.Position.PICKUP);
+		super.vertical((153.5/2)-(45/2)+(13/2),Arm.Position.PICKUP,true);
+		super.verticalReverse((153.5/2)-(45/2)+(13/2), Arm.Position.TRAVEL, false);
+		super.rotate(90, false);
+		super.vertical(56/2+super.CenterRobotLength,Arm.Position.SWITCH, false);
+		super.rotate(90, true,Arm.Position.SWITCH);
+		super.vertical(super.CenterRobotCorner);
+		intake.autonOuttake();
+	}
+
+
+}
